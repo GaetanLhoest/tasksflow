e3f4663f7fc61776fc7001cf58e5143666ad29a9
Gaetan L'Hoest <gaetan.lhoest@gmail.com>
2020-11-03 23:22:55 +0100

    Get user input for issue id

--- src/commands/issue.ts
+++ src/commands/issue.ts
@@ -1,5 +1,11 @@
 import * as vscode from "vscode";
 
-export function issue() {
+export async function issue() {
   vscode.window.showInformationMessage("Starting to work on a given issue!");
+  let issueId;
+  issueId = await vscode.window.showInputBox({
+    ignoreFocusOut: true,
+    placeHolder: "E.g. 25",
+    prompt: "Id of the issue",
+  });
 }
